cc8360fe86412d9a29693acdcaffc04dd6e79e28
aydotjs <ayobami66@gmail.com>
2023-07-04 23:22:38 +0100

    m

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,4 +2,5 @@ const express = require("express");
 const authRouter = express.Router();
 const { createUser } = require("../controller/userControl");
 authRouter.route("/").post("/register", createUser);
+console.log(authRouter);
 module.exports = authRouter;
