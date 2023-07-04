a0ad3141f158299c9e2c54b28144cf88de73f651
aydotjs <ayobami66@gmail.om>
2023-07-04 11:19:36 +0100

    controllers

---
+++ controller/userControl.js
@@ -0,0 +1,17 @@
+const User = require("../model/userModel");
+
+const createUser = async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne(email);
+  if (!findUser) {
+    // create a new user
+    const newUser = User.create(req.body);
+    res.json(newUser);
+  } else {
+    // user already exist
+    res.json({
+      msg: "User already exists",
+      status: "fail",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// importing essential packages
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect");
+const authRouter = require("./routes/authRoute")
+// connecting to  database
 dbConnect();
+// reading environment variables
 dotenv.config({
   path: "./config.env",
 });
+// parsing json from request body
+app.use(express.json())
 const PORT = process.env.PORT || 7000;
 
+app.use("/api/user", authRouter)
 app.use("/", (req, res) => {
   res.send("Hello from the server side");
 });
+// starting up my server
 app.listen(PORT, () => {
   console.log(`SERVER HAS STARTED LISTENING ON PORT ${PORT}`);
 });
